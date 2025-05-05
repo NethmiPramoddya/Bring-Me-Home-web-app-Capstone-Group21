8e5e2c71efc4c18683dd89046c8906c263c95ab6
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-05-05 23:01:39 +0530

    homepage_created

---
+++ client/src/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+export default function Home() {
+  return (
+    <div>
+       <div class="w-[95%] h-[651px] flex-shrink-0 rounded-[68px] bg-[#F0F0F0] absolute top-[120px] ml-[30px]"></div>
+    </div>
+  )
+}
