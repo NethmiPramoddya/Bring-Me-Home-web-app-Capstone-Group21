59e882e299b9b9fddb197a11ddaec2a1e8fe2c26
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-05-08 19:10:29 +0530

    who are we added with image

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import TopImage from "./assets/home_page_img.png";
+import Bag from "./assets/bag.png";
+
 
 export default function Home() {
   return (
@@ -12,15 +14,15 @@ export default function Home() {
           Home
         </h1>
         <div>
-        <img
+          <img
             class="w-[300px] h-[300px]
-            sm:w-[700px] sm:h-[700px] sm:top-[-120px] sm:left-[300px]
-            md:w-[750px] md:h-[750px] md:top-[-90px] md:left-[300px]
-            2md:w-[800px] 2md:h-[800px] 2md:top-[-125px] 2md:left-[300px]
-            3md:w-[900px] 3md:h-[900px] 3md:top-[-140px] 3md:left-[350px]
-            4md:w-[900px] 4md:h-[900px] 4md:top-[-145px] 4md:left-[360px]
-            lg:w-[1000px] lg:h-[1000px] lg:top-[-145px] lg:left-[370px]
-            xl:w-[1100px] xl:h-[1100px] xl:top-[-150px] xl:left-[440px]
+            sm:w-[550px] sm:h-[550px] sm:top-[-120px] sm:left-[300px]
+            md:w-[600px] md:h-[600px] md:top-[-90px] md:left-[300px]
+            2md:w-[650px] 2md:h-[650px] 2md:top-[-125px] 2md:left-[300px]
+            3md:w-[700px] 3md:h-[700px] 3md:top-[-140px] 3md:left-[350px]
+            4md:w-[700px] 4md:h-[700px] 4md:top-[-145px] 4md:left-[360px]
+            lg:w-[800px] lg:h-[800px] lg:top-[-145px] lg:left-[370px]
+            xl:w-[800px] xl:h-[800px] xl:top-[-150px] xl:left-[440px]
             
             flex-shrink-0 aspect-[1/1] absolute top-[-100px] left-[50px]"
             src={TopImage}
@@ -34,12 +36,22 @@ export default function Home() {
           helping traveler.
         </p>
         <button class="w-[180px] h-[60px] sm:w-[220px] sm:h-[65px] md:w-[242px] md:h-[71px] flex-shrink-0 rounded-[13px] bg-[#6A6A6A] absolute top-[420px] left-[40px] sm:top-[480px] sm:left-[70px] md:top-[505px] md:left-[85px]">
-          <p  class="w-[90px] h-[24px] text-white font-[Inter] text-[18px] sm:w-[115px] sm:h-[29px] sm:text-[20px] md:text-[24px] font-bold leading-none absolute top-[10px] left-[20px] sm:top-[20px] sm:left-[58px]">
+          <p class="w-[90px] h-[24px] text-white font-[Inter] text-[18px] sm:w-[115px] sm:h-[29px] sm:text-[20px] md:text-[24px] font-bold leading-none absolute top-[10px] left-[20px] sm:top-[20px] sm:left-[58px]">
             Get Start
           </p>
         </button>
       </div>
+      <div className="wd">
+        <h1 class="w-[472px] h-[104px] text-[#6A6A6A] font-inter text-[64px] font-extrabold leading-none absolute top-[900px] ml-[50%]">Who we are...?</h1>
+        <p class="w-[627px] h-[375px] flex-shrink-0 text-[#6A6A6A] font-bold text-[32px] font-[Bellota] absolute top-[1000px] ml-[50%]">
+          Bring Me Home is a trusted global platform that connects people across
+          borders by matching package senders with reliable travelers. We make
+          it easy to send personal items, gifts, or essentials internationally,
+          ensuring a safe and human touch in every delivery—powered by community
+          trust and a shared purpose.
+        </p>
+        <img class="w-[356px] h-[463px] aspect-[356/463] absolute top-[950px] ml-[10%]" src={Bag} alt="bag"/>
+      </div>
     </div>
   );
 }
-
