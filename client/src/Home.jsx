336b6aa548db83535d8ae74e5983f156b5e4d246
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-05-08 16:45:50 +0530

    respomsive_added_top_1

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -13,7 +13,16 @@ export default function Home() {
         </h1>
         <div>
           <img
-            class="w-[959px] h-[959px] flex-shrink-0 aspect-[1/1] absolute top-[-180px] left-[440px]"
+            class="w-[300px] h-[300px]
+            sm:w-[600px] sm:h-[600px] sm:top-[-90px] sm:left-[400px]
+            md:w-[650px] md:h-[650px] md:top-[-90px] md:left-[400px]
+            2md:w-[750px] 2md:h-[750px] 2md:top-[-135px] 2md:left-[330px]
+            3md:w-[800px] 3md:h-[800px] 3md:top-[-140px] 3md:left-[350px]
+            4md:w-[800px] 4md:h-[800px] 4md:top-[-140px] 4md:left-[350px]
+            lg:w-[850px] lg:h-[850px] lg:top-[-145px] lg:left-[370px]
+            xl:w-[900px] xl:h-[900px] xl:top-[-150px] xl:left-[440px]
+            
+            flex-shrink-0 aspect-[1/1] absolute top-[-100px] left-[50px]"
             src={TopImage}
             alt="plane"
           />
@@ -24,8 +33,8 @@ export default function Home() {
           your loved ones abroad—with a personal touch and a small tip for the
           helping traveler.
         </p>
-        <button class="w-[242px] h-[71px] flex-shrink-0 rounded-[13px] bg-[#6A6A6A] absolute top-[505px] left-[85px]">
-          <p class="w-[115px] h-[29px] flex-shrink-0 text-white font-[Inter] text-[24px] font-bold leading-none absolute top-[23px] left-[58px]">
+        <button class="w-[180px] h-[60px] sm:w-[220px] sm:h-[65px] md:w-[242px] md:h-[71px] flex-shrink-0 rounded-[13px] bg-[#6A6A6A] absolute top-[420px] left-[40px] sm:top-[480px] sm:left-[70px] md:top-[505px] md:left-[85px]">
+          <p  class="w-[90px] h-[24px] text-white font-[Inter] text-[18px] sm:w-[115px] sm:h-[29px] sm:text-[20px] md:text-[24px] font-bold leading-none absolute top-[10px] left-[20px] sm:top-[20px] sm:left-[58px]">
             Get Start
           </p>
         </button>
@@ -33,3 +42,4 @@ export default function Home() {
     </div>
   );
 }
+
