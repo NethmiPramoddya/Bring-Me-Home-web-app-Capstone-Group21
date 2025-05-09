deaec5572831dca76f8a982153f6236b4a23d74e
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-05-09 16:52:28 +0530

    scroll smooth animation added for home pg

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -4,14 +4,20 @@ import TopImage from "./assets/home_page_img.png";
 import Bag from "./assets/bag.png";
 import Shuttle from "./assets/shuttle.png";
 import Safe_logo from "./assets/safe.png";
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
 
 export default function Home() {
+
+  useEffect(() => {
+    AOS.init({duration: 1300});
+  })
   return (
     <div>
-      <div class="w-[95%] h-[645px] flex-shrink-0 rounded-[68px] bg-[#F0F0F0] absolute top-[120px] ml-[30px]">
-        <h1 class="w-[448px] h-[403px] flex-shrink-0 text-[#FF8B2A] font-[Aclonica] text-[96px] font-normal leading-none absolute top-[85px] ml-[85px]">
+      <div class=" w-[95%] h-[645px] flex-shrink-0 rounded-[68px] bg-[#F0F0F0] absolute top-[120px] ml-[30px] data-aos='fade-up'" data-aos="fade-up">
+        <h1 class="w-[448px] h-[403px] flex-shrink-0 text-[#FF8B2A] font-[Aclonica] text-[96px] font-normal leading-none absolute top-[85px] ml-[85px]" data-aos="fade-up">
           Bring Me
           <br />
           Home
@@ -30,9 +36,10 @@ export default function Home() {
             flex-shrink-0 aspect-[1/1] absolute top-[-100px] left-[50px]"
             src={TopImage}
             alt="plane"
+            data-aos="fade-left"
           />
         </div>
-        <p class="w-[508px] h-[124px] flex-shrink-0 text-black font-[Bellota] text-[20px] font-normal leading-none absolute top-[390px] ml-[85px]">
+        <p class="animate-pulse w-[508px] h-[124px] flex-shrink-0 text-black font-[Bellota] text-[20px] font-normal leading-none absolute top-[390px] ml-[85px]">
           Bring Me Home connects people across borders by matching package
           senders with trusted travelers. Easily send gifts or essentials to
           your loved ones abroad—with a personal touch and a small tip for the
@@ -45,10 +52,10 @@ export default function Home() {
         </button>
       </div>
       <div className="wd">
-        <h1 class="w-[472px] h-[104px] text-[#6A6A6A] font-inter text-[64px] font-extrabold leading-none absolute top-[900px] ml-[50%]">
+        <h1 class=" w-[472px] h-[104px] text-[#6A6A6A] font-inter text-[64px] font-extrabold leading-none absolute top-[900px] ml-[50%]" data-aos="fade-up">
           Who we are...?
         </h1>
-        <p class="w-[627px] h-[375px] flex-shrink-0 text-[#6A6A6A] font-bold text-[32px] font-[Bellota] leading-normal absolute top-[1000px] ml-[50%] text-justify">
+        <p class=" w-[627px] h-[375px] flex-shrink-0 text-[#6A6A6A] font-bold text-[32px] font-[Bellota] leading-normal absolute top-[1000px] ml-[50%] text-justify"  data-aos="fade-up">
           Bring Me Home is a trusted global platform that connects people across
           borders by matching package senders with reliable travelers. We make
           it easy to send personal items, gifts, or essentials internationally,
@@ -59,13 +66,14 @@ export default function Home() {
           class="w-[356px] h-[463px] aspect-[356/463] absolute top-[950px] ml-[10%]"
           src={Bag}
           alt="bag"
+          data-aos="fade-left"
         />
       </div>
       <div>
-        <h1 class="w-[472px] h-[104px] shrink-0 text-[#6A6A6A] font-inter text-[64px] not-italic font-extrabold leading-none absolute top-[1600px] ml-[100px]">
+        <h1 class="w-[472px] h-[104px] shrink-0 text-[#6A6A6A] font-inter text-[64px] not-italic font-extrabold leading-none absolute top-[1600px] ml-[100px]" data-aos="fade-up">
           Is It Fast...?
         </h1>
-        <p class="w-[627px] h-[375px] flex-shrink-0 text-[#6A6A6A] font-[Bellota] text-[32px] not-italic font-bold leading-normal absolute top-[1700px] ml-[100px] text-justify">
+        <p class="w-[627px] h-[375px] flex-shrink-0 text-[#6A6A6A] font-[Bellota] text-[32px] not-italic font-bold leading-normal absolute top-[1700px] ml-[100px] text-justify" data-aos="fade-up">
           Yes! Bring Me Home offers a faster alternative to traditional
           shipping. By connecting with travelers already heading to your
           package’s destination, your items can be delivered in less time—often
@@ -76,13 +84,14 @@ export default function Home() {
           class="w-[440px] h-[452px] shrink-0 aspect-[145/149] absolute top-[1600px] ml-[60%]"
           src={Shuttle}
           alt="shuttle"
+          data-aos="fade-right"
         />
       </div>
       <div>
-        <h1 class="w-[472px] h-[104px] text-[#6A6A6A] font-inter text-[64px] font-extrabold leading-none absolute top-[2200px] ml-[50%]">
+        <h1 class="w-[472px] h-[104px] text-[#6A6A6A] font-inter text-[64px] font-extrabold leading-none absolute top-[2200px] ml-[50%]" data-aos="fade-up">
           Why we are Secure...?
         </h1>
-        <p class="w-[627px] h-[375px] flex-shrink-0 text-[#6A6A6A] font-bold text-[32px] font-[Bellota] leading-normal absolute top-[2350px] ml-[50%] text-justify">
+        <p class="w-[627px] h-[375px] flex-shrink-0 text-[#6A6A6A] font-bold text-[32px] font-[Bellota] leading-normal absolute top-[2350px] ml-[50%] text-justify" data-aos="fade-up">
           At Bring Me Home, your package’s safety is our top priority. Every
           delivery is arranged directly between you and a verified traveler,
           with the package securely wrapped and checked in person—ensuring full
@@ -95,8 +104,10 @@ export default function Home() {
           class="w-[434px] h-[434px] aspect-[1/1] absolute top-[2380px] ml-[10%]"
           src={Safe_logo}
           alt="bag"
+          data-aos="fade-down"
         />
       </div>
+      
     </div>
   );
 }
