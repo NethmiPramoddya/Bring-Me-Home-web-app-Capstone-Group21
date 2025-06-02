83fa7e443b41d891fba6a21fa134eae438cb039e
NethmiPramoddya <nethmik2001@gmail.com>
2025-06-02 19:27:14 +0530

    implement chat bot using groq and gemma model

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,7 @@ import RoomId from './RoomId'
 import OnGoingTasks from './OnGoingTasks'
 import ReceiverDetails from './ReceiverDetails'
 import EditProfile from './EditProfile'
+import ChatBot from './ChatBot'
 
 function App() {
   const userId = localStorage.getItem('userId');
@@ -26,31 +27,30 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
-      
-      <Routes>
-
-        <Route path='/' element={<UsersMyRequestes/>}></Route>
-        <Route path='/Home' element={<Home/>}></Route>
-        <Route path='/create' element={<CreateUsersForm/>}></Route>
-        <Route path='/edit/:id' element={<EditUsers/>}></Route>
-        <Route path='/register' element={<SignUp/>}></Route>
-        <Route path='/login' element={<LogIn/>}></Route>
-        <Route path='/become-traveler' element={<TravelerForm/>}></Route>
-        <Route path='/profile/:id' element={<Profile/>}></Route>
-        {/* <Route path="/buyer-requests/:senderRequestId" element={< RequestDetails/>} /> */}
-        <Route path='/notifications' element={<Notifications userId={userId} />}></Route>
-        <Route path="/more_info/:id" element={<MoreInfo/>} /> 
-        <Route path='/mySenderRequests/:id' element={<MySenderRequests/>}></Route>
-        <Route path='/travelingData/:id' element={<MyTravelingData/>}></Route>
-        <Route path='/view_more/:id' element={<ViewMore/>}></Route>
-        <Route path="/chat/:roomId" element={<RoomId />} />
-        <Route path='/onGoingTasks/:id' element={<OnGoingTasks/>}></Route>
-        <Route path="/receiver/:id" element={<ReceiverDetails />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-
-      </Routes>
-      
+        <Navbar/>
+        
+        <Routes>
+          <Route path='/' element={<UsersMyRequestes/>}></Route>
+          <Route path='/Home' element={<Home/>}></Route>
+          <Route path='/create' element={<CreateUsersForm/>}></Route>
+          <Route path='/edit/:id' element={<EditUsers/>}></Route>
+          <Route path='/register' element={<SignUp/>}></Route>
+          <Route path='/login' element={<LogIn/>}></Route>
+          <Route path='/become-traveler' element={<TravelerForm/>}></Route>
+          <Route path='/profile/:id' element={<Profile/>}></Route>
+          {/* <Route path="/buyer-requests/:senderRequestId" element={< RequestDetails/>} /> */}
+          <Route path='/notifications' element={<Notifications userId={userId} />}></Route>
+          <Route path="/more_info/:id" element={<MoreInfo/>} /> 
+          <Route path='/mySenderRequests/:id' element={<MySenderRequests/>}></Route>
+          <Route path='/travelingData/:id' element={<MyTravelingData/>}></Route>
+          <Route path='/view_more/:id' element={<ViewMore/>}></Route>
+          <Route path="/chat/:roomId" element={<RoomId />} />
+          <Route path='/onGoingTasks/:id' element={<OnGoingTasks/>}></Route>
+          <Route path="/receiver/:id" element={<ReceiverDetails />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+        </Routes>
+        
+        <ChatBot />
       </BrowserRouter>
     </div>
   )
