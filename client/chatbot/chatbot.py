0ffa275c881adb97aca64fb4826532f0475a8379
NethmiPramoddya <nethmik2001@gmail.com>
2025-07-16 20:23:57 +0530

    Revert "Python based ChatBot terminal integration change2"
    This reverts commit a821c68e83148ad4da221c88da65183fc69a3542.

--- client/chatbot/chatbot.py
+++ client/chatbot/chatbot.py
@@ -31,16 +31,3 @@ def stream_graph_updates(user_input: str):
         for value in event.values():
             print("Assistant:", value["messages"][-1]["content"])
 
-if __name__ == "__main__":
-    while True:
-        try:
-            user_input = input("User: ")
-            if user_input.lower() in ["quit", "exit", "q"]:
-                print("Goodbye!")
-                break
-
-            stream_graph_updates(user_input)
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            break            
-
